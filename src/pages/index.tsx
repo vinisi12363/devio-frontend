0ec05e9e370a0eebfee50ce8ddeca76ce428dde8
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2024-01-23 17:04:47 -0300

    refactor page import structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export { default as HomePage } from './HomePage/HomePage';
+export { default as KitchenPage } from './KitchenPage/KitchenPage';
+export { default as TakeOutPage} from './TakeOutPage/TakeOutPage';
