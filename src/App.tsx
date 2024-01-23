0b1c5ad3f6a1a62997c25e306f96155526341f50
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2024-01-23 16:56:19 -0300

    refactor: refatorando imports das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import HomePage from "./pages/HomePage/index";
-import KitchenPage from "./pages/KitchenPage/index";
-import TakeOutPage from "./pages/TakeoutPage/index";
+import HomePage from "./pages/HomePage/index.tsx";
+import KitchenPage from "./pages/KitchenPage/index.tsx";
+import TakeOutPage from "./pages/TakeoutPage/index.tsx";
 import { CategoryProvider } from "./Contexts/CategoryContext";
 import { ProductProvider } from "./Contexts/ProductContext";
 import { OrderProvider } from "./Contexts/OrderContext";
